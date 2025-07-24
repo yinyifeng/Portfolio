67bcf7ea3ae7965620775c7e47339f1dead72d11
Yinyi <yinyi.gt@gmail.com>
2025-07-23 21:34:35 -0400

    Increased speed of typewriter

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const originalText = typedText.textContent;
 typedText.textContent = ''; // Clear it so we can type it
 const learnMoreBtn = document.getElementById("learn-more");
 
-const speed = 60;
+const speed = 50;
 
 function typeWriter() {
   if (i < originalText.length) {
