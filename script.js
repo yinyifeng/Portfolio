ab3fd61fc029b8c3db4c1f2a2b54cf15f435945f
Yinyi <yinyi.gt@gmail.com>
2025-07-23 21:32:12 -0400

    Centering intro text on home page

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ let i = 0;
 const typedText = document.getElementById("typed-text");
 const originalText = typedText.textContent;
 typedText.textContent = ''; // Clear it so we can type it
+const learnMoreBtn = document.getElementById("learn-more");
 
-const speed = 80;
+const speed = 60;
 
 function typeWriter() {
   if (i < originalText.length) {
     typedText.textContent += originalText.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
+  } else {
+    learnMoreBtn.style.display = "inline-block"; // Show the button
   }
 }
 
